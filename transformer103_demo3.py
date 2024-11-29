10aee5cf4617173f90e843f64fb467f337451f1a
vipandyc <vipandyc@mit.edu>
2024-11-28 19:53:01 -0500

    geegeegee

---
+++ transformer103_demo3.py
@@ -0,0 +1,385 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import numpy as np
+import copy
+import pandas as pd
+from tqdm import tqdm
+from os.path import exists
+from os import remove, chdir
+import pickle
+from matplotlib import pyplot as plt
+#from torch.utils.tensorboard import SummaryWriter
+#from synthesizer import parse_csv, synthesize
+
+DEVICE = "cuda"
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+        
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+        
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+        output = torch.matmul(attn_probs, V)
+        return output
+        
+    def split_heads(self, x):
+        batch_size, seq_length, d_model = x.size()
+        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
+        
+    def combine_heads(self, x):
+        batch_size, _, seq_length, d_k = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
+        
+    def forward(self, Q, K, V, mask=None):
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+        
+        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+        output = self.W_o(self.combine_heads(attn_output))
+        return output
+    
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model):
+        super(PositionalEncoding, self).__init__()
+        self.d_model = d_model
+
+    def forward(self, x):
+        max_len = x.size(1)
+        pe = torch.zeros(max_len, self.d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * -(math.log(10000.0) / self.d_model))
+        
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        pe = pe.unsqueeze(0).to(x.device)
+        return x + pe
+
+
+class DecoderPositionalEncoding(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.d_model = d_model
+
+    def forward(self, x, tgt):
+        # max_len = x.size(1)
+        tgt_one_hot = tgt[:, :, 12:]
+        tgt_class = torch.argmax(tgt_one_hot, dim=-1)
+        pe = torch.zeros_like(x)
+        position = torch.cumsum(tgt_class, dim=1).unsqueeze(-1)
+        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * -(math.log(10000.0) / self.d_model)).to(position.device)
+        
+        pe[:, :, 0::2] = torch.sin(position * div_term)
+        pe[:, :, 1::2] = torch.cos(position * div_term)
+        return x + pe
+
+    
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, mask):
+        attn_output = self.self_attn(x, x, x, mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm2(x + self.dropout(ff_output))
+        return x
+    
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(DecoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.cross_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        attn_output = self.self_attn(x, x, x, tgt_mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        attn_output = self.cross_attn(x, enc_output, enc_output, src_mask)
+        x = self.norm2(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm3(x + self.dropout(ff_output))
+        return x
+
+class EmbedHead(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        inner_dim_1,
+        inner_dim_2,
+        out_dim
+    ):
+        super().__init__()
+        self.linear1 = nn.Linear(input_dim, inner_dim_1)
+        self.linear2 = nn.Linear(inner_dim_1, inner_dim_2)
+        self.linear3 = nn.Linear(inner_dim_2, out_dim)
+        self.activation_fn = nn.functional.gelu
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation_fn(x)
+        x = self.linear2(x)
+        x = self.activation_fn(x)
+        x = self.linear3(x)
+        return x
+    
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
+        super(Transformer, self).__init__()
+        self.encoder_embedding = EmbedHead(src_vocab_size, d_model, d_model, d_model)
+        self.decoder_embedding = EmbedHead(tgt_vocab_size + 9, d_model, d_model, d_model)
+        self.positional_encoding = PositionalEncoding(d_model)
+        self.decoder_positional_encoding = DecoderPositionalEncoding(d_model)
+
+        self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+
+        self.fc = nn.Linear(d_model, tgt_vocab_size + 9)
+        self.dropout = nn.Dropout(dropout)
+
+    def generate_mask(self, src, tgt):
+        src_mask = (torch.sum(src, dim=2) > 0).unsqueeze(1).unsqueeze(2)
+        tgt_mask = (torch.sum(tgt, dim=2) > 0).unsqueeze(1).unsqueeze(3)
+        seq_length = tgt.size(1)
+        d = 1
+        # if self.training:
+        #     d = torch.randint(-14, 2, (1,)).item()
+        # else:
+        #     d = 1
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=d)).bool().to(DEVICE)
+        tgt_mask = tgt_mask & nopeak_mask
+        return src_mask, tgt_mask
+
+    def forward(self, src, tgt):
+        src_mask, tgt_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded = self.dropout(self.decoder_positional_encoding(self.decoder_embedding(tgt), tgt))
+
+        enc_output = src_embedded
+        for enc_layer in self.encoder_layers:
+            enc_output = enc_layer(enc_output, src_mask)
+
+        dec_output = tgt_embedded
+        for dec_layer in self.decoder_layers:
+            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+
+        output = self.fc(dec_output)
+        return torch.cat([torch.sigmoid(output[:,:,:12]), output[:,:,12:]], dim=-1)
+
+if __name__ == "__main__":
+    chdir("POP909/model_train")
+    src_vocab_size = 12
+    tgt_vocab_size = 12
+    d_model = 512
+    num_heads = 8
+    num_layers = 4
+    d_ff = 4096//8
+    max_seq_length = 2400
+    dropout = 0.1
+    batchsize = 16
+    mode = input('Mode=train/inference/debugging?GEEE! : ')
+
+    #writer = SummaryWriter('.log')
+
+    transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout).to(DEVICE)
+
+    # load data
+    if exists("new_trainset2.pkl") and exists("new_validset2.pkl") and exists("new_testset2.pkl"):
+        print("splitted dataset found!")
+        with open("new_trainset2.pkl", "rb") as f:
+            trainset = pickle.load(f)
+        with open("new_validset2.pkl", "rb") as f:
+            validset = pickle.load(f)
+        with open("new_testset2.pkl", "rb") as f:
+            testset = pickle.load(f)
+    else:
+        print("?")
+    
+    def collate_fn(batch):
+        # Unpack batch into individual components
+        idx, src_data, tgt_data = zip(*batch)
+        #print(len(rates[0]), len(tgt_data[0]), len(src_data[0]))
+        
+        # Convert `src_data`, `tgt_data`, and `rates` to tensors if they are not already
+        src_data = [torch.tensor(s, dtype=torch.float32) if not isinstance(s, torch.Tensor) else s for s in src_data]
+        tgt_data = [torch.tensor(t, dtype=torch.float32) if not isinstance(t, torch.Tensor) else t for t in tgt_data]
+
+        src_data = [torch.cat([s], dim=-1) for s in src_data]
+        tgt_data = [torch.cat([t], dim=-1) for t in tgt_data]
+
+        # Pad src_data
+        src_data = nn.utils.rnn.pad_sequence(src_data, batch_first=True, padding_value=0.).to(DEVICE)
+
+        # Pad tgt_data
+        tgt_data_padded = nn.utils.rnn.pad_sequence(tgt_data, batch_first=True, padding_value=-1).to(DEVICE)
+
+        # Extract the last dimension and one-hot encode it
+        tgt_last_dim = tgt_data_padded[:, :, -1]  # Shape: (batch_size, sequence_length)
+        num_classes = 9  # Number of classes (0-8)
+        tgt_last_dim_onehot = nn.functional.one_hot(torch.clamp(tgt_last_dim, min=0).long(), num_classes=num_classes).float()
+          # Reset padding values to zeros in one-hot representation
+
+        # Concatenate one-hot encoding with the remaining dimensions of tgt_data
+        tgt_data_remaining = tgt_data_padded[:, :, :-1]  # Exclude the last dimension
+        tgt_data_concat = torch.cat([tgt_data_remaining, tgt_last_dim_onehot], dim=-1)  # Concatenate along the last axis
+        tgt_data_concat[tgt_last_dim == -1] = 0.
+
+        tgt_cls_label = tgt_last_dim.clone().long()
+        tgt_cls_label[tgt_last_dim == -1] = 0
+        return idx, src_data, tgt_data_concat, tgt_cls_label
+
+    
+    validset = data.DataLoader(validset, batch_size=1, collate_fn=collate_fn)
+    testset = data.DataLoader(testset, batch_size=1, collate_fn=collate_fn)
+
+    criterion = nn.BCELoss(reduction="mean")
+    loss_new = nn.CrossEntropyLoss()
+    if mode == "train": 
+        trainset = data.DataLoader(trainset, batch_size=batchsize, collate_fn=collate_fn)
+
+        optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+        transformer.train()
+        k = 1
+        for epoch in tqdm(range(200)):
+            for i, pair_data in tqdm(enumerate(trainset)):
+                idx, src_data, tgt_data, tgt_cls_label = pair_data
+                optimizer.zero_grad()
+                # Use tgt_data as input to the transformer
+                output = transformer(src_data, tgt_data[:, :-1, :])
+                # print(output.shape, tgt_data.shape)
+                loss1 = criterion(output[:, :, :12].contiguous().view(-1), tgt_data[:, 1:, :12].contiguous().view(-1))
+                #print(tgt_data[:, 1:, -1])
+                #print(output[:, :, 12:].reshape(-1, 9))
+                #print(tgt_data[:, 1:, 12:].reshape(-1, 9))
+                #print(tgt_cls_label[:, 1:])
+                loss2 = loss_new(output[:, :, 12:].reshape(-1, 9), tgt_cls_label[:, 1:].reshape(-1))
+
+                #weights = 1 + tgt_data[:, 1:, -1]  # Example: Give higher weight to higher values
+                #loss2 = nn.functional.mse_loss(torch.sigmoid(output[:, :, -1]), tgt_data[:, 1:, -1]/8, reduction='none')
+                #loss2 = (loss2 * weights).mean()
+
+                loss = loss1 + k * loss2
+                
+                loss.backward()
+                optimizer.step()
+                '''
+                # Match the shapes for loss calculation
+                output = output.contiguous().view(-1)
+                target = tgt_data_with_rates[:, 1:, :].contiguous().view(-1)
+
+                # Ensure target and output have the same shape
+                min_len = min(output.size(0), target.size(0))
+                output = output[:min_len]
+                target = target[:min_len]
+
+                loss = criterion(output, target)
+                loss.backward()
+                optimizer.step()'''
+
+                #writer.add_scalar("loss", loss, global_step=epoch + (i + 1) * batchsize / 710)
+            print(f"Epoch: {epoch+1}, Loss1: {loss1.item()}, Loss2: {loss2.item()}, k: {k}, Loss: {loss.item()}")
+            k *= 0.96
+            if (epoch + 1) % 10 == 0:
+                torch.save(transformer, f"model_{epoch + 1}_regularized.pt")
+    
+    elif mode == 'inference':
+        # inference step
+        transformer.eval()
+        transformer.load_state_dict(torch.load("model_100_regularized.pt").state_dict())
+        tot_loss = 0
+
+        ## for fake testing only
+        trainset = data.DataLoader(trainset, batch_size=1, collate_fn=collate_fn)
+        ###
+        
+        for i, pair_data in tqdm(enumerate(testset)):
+            if i == 3:
+                break
+            idx, src_data, tgt_data, tgt_cls_label = pair_data
+            idx = idx[0]
+            #generated_tgt = []
+            #print(tgt_data_with_rates.shape)
+            current_tgt = tgt_data[:, :1, :]  # Use the first step as a starting point
+
+            for t in range(1, tgt_data.size(1)):  # Generate timestep by timestep
+                # print(src_data.shape, current_tgt.shape)
+                output = transformer(src_data, current_tgt).detach()
+                #print(output.shape)
+                probabilities = output[:, -1, 12:] # [batch, 1, 9]
+                chord_prediction = output[:, -1, :12]  # Assuming other features are `tgt_data`
+
+                #probabilities = torch.softmax(probabilities, dim=-1)  # First 12 values are probabilities
+                chord_prediction = torch.bernoulli(chord_prediction)  # Randomly sample 0 or 1 based on probabilities
+                length_class = torch.argmax(probabilities, dim=-1)
+                onehot_encoding = nn.functional.one_hot(length_class, num_classes=9).float()
+
+                # generated_tgt.append(next_tgt)
+                token = torch.cat([chord_prediction, onehot_encoding], dim=-1)
+                current_tgt = torch.cat([current_tgt, token.unsqueeze(1)], dim=1)
+
+            # Compute loss if necessary
+            # loss = criterion(current_tgt[:, 1:, :].contiguous().view(-1), tgt_data_with_rates[:, 1:, :].contiguous().view(-1))
+            # tot_loss += loss.item()
+
+            torch.save(current_tgt, f'../../song_{idx}_regularized.pt')
+
+    elif mode == 'debugging':
+        # inference step
+        transformer.eval()
+        transformer.load_state_dict(torch.load("model_30.pt").state_dict())
+        tot_loss = 0
+
+        for i, pair_data in tqdm(enumerate(testset)):
+            if i == 3:
+                break
+            idx, src_data, tgt_data_with_rates = pair_data
+            # Use tgt_data_with_rates as input to the transformer
+            output = transformer(src_data, tgt_data_with_rates[:, :-1, :])
+            #print(output.shape, tgt_data_with_rates.shape)
+            loss = criterion(output.contiguous().view(-1), tgt_data_with_rates[:, 1:, :].contiguous().view(-1))
+            print(loss)
+            t1 = output[0, :, -1].contiguous().view(-1).detach().cpu().numpy()
+            t2 = tgt_data_with_rates[0, 1:, -1].contiguous().view(-1).detach().cpu().numpy()
+            N = len(t1)
+            plt.figure(figsize=(12, 3))
+            plt.plot(range(N), t1, label='prediction')
+            plt.plot(range(N), t2, label='truth')
+            plt.legend()
+            plt.savefig(f'{i}.jpg')
+            plt.show()
